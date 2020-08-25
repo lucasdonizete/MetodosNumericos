a04174e3b120fe904880de5f446ee667e78040c5
lucasdonizete <70209644+lucasdonizete@users.noreply.github.com>
2020-08-25 10:05:22 -0300

    Create listann.py

---
+++ listann.py
@@ -0,0 +1,11 @@
+ # lista exemplo
+nn=6
+ def lista(nn):
+    y=[]
+    for i in range(nn):
+      x=[]
+      for j in range(nn):
+        x.append(j)
+      y.append(x)
+    return y
+                
